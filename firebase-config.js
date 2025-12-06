197ec1889a4efcc342f037746f10976a52d71d01
FernandoCaldeira <fernandonunescaldeira@gmail.com>
2025-12-06 14:06:03 +0000

    Update Firebase configuration values

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,12 @@
 // You can find these values in Firebase Console > Project Settings > Your apps > SDK setup and configuration
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyA8f_7kiDyBS5213ClTwyYogLYOOB_6VS0",
-    authDomain: "voz-dos-riachos.firebaseapp.com",
-    projectId: "voz-dos-riachos",
-    storageBucket: "voz-dos-riachos.firebasestorage.app",
-    messagingSenderId: "129153742502",
-    appId: "1:129153742502:web:1f57da77bf1522b7d7e667"
+    apiKey: "AIzaSyA8f_7kiDyBS52i3ClTwy7ogLYOOB_6VSQ",
+  authDomain: "voz-dos-riachos.firebaseapp.com",
+  projectId: "voz-dos-riachos",
+  storageBucket: "voz-dos-riachos.firebasestorage.app",
+  messagingSenderId: "129153742502",
+  appId: "1:129153742502:web:1f57da77bf1522b7d7e667"
 };
 
 // INSTRUCTIONS:
